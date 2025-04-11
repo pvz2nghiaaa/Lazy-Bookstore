1b25f6b0ee900db0fc3fccf2c3d9d6032f3d8b48
pvz2nghiaaa <thanhnghia310707@gmail.com>
2025-04-11 18:23:49 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+export const frontend_url = "beach-rogers-sacramento-proceeds.trycloudflare.com";
+export const admin_url = "academic-sister-rca-marine.trycloudflare.com";
+export const backend_url = "https://promotional-put-fate-lee.trycloudflare.com";
